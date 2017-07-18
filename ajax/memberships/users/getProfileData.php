bb3431f572e284db33d97a8641ffdf7f722f9a0d
Patrick Müller <p.mueller@pcsg.de>
2017-07-18 13:03:57 +0200

    refactor: Always show default membership on last position

--- ajax/memberships/users/getProfileData.php
+++ ajax/memberships/users/getProfileData.php
@@ -1,7 +1,7 @@
 <?php
 
 use QUI\Memberships\Users\Handler as MembershipUsersHandler;
-use QUI\Memberships\Users\MembershipUser;
+use QUI\Memberships\Handler as MembershipsHandler;
 
 /**
  * Get all MembershipUser Objects data for the current session user (for frontend)
@@ -21,6 +21,25 @@ QUI::$Ajax->registerFunction(
 
             foreach ($membershipUsers as $MembershipUser) {
                 $data[] = $MembershipUser->getFrontendViewData();
+            }
+
+            // put default membership last
+            $DefaultMembership = MembershipsHandler::getDefaultMembership();
+
+            if ($DefaultMembership !== false) {
+                $defaultId = $DefaultMembership->getId();
+
+                usort($data, function ($a, $b) use ($defaultId) {
+                    if ($a['membershipId'] == $defaultId) {
+                        return 1;
+                    }
+
+                    if ($b['membershipId'] == $defaultId) {
+                        return -1;
+                    }
+
+                    return 0;
+                });
             }
         } catch (\Exception $Exception) {
             QUI\System\Log::addError('AJAX :: package_quiqqer_memberships_ajax_memberships_users_getHistory');
